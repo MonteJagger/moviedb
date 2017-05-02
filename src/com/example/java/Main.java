09991296433a9ca414e437e62104e49ba1ec6cd7
Hiumathy <Hiumathy@gmail.com>
2017-05-01 23:57:07 -0500

    clean

--- src/com/example/java/Main.java
+++ src/com/example/java/Main.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
