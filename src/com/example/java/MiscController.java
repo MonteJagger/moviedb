da47ba8201573dbb035e8025fb08ab75a5a99fcb
Hiumathy <Hiumathy@gmail.com>
2017-05-04 06:51:38 -0500

    MiscNeedsQueries

--- src/com/example/java/MiscController.java
+++ src/com/example/java/MiscController.java
@@ -1,5 +1,7 @@
 package com.example.java;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,7 +10,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -21,14 +25,32 @@ import java.util.ResourceBundle;
  * Created by Hiumathy on 5/3/17.
  */
 public class MiscController implements Initializable{
-    // button IDs
+    // page buttons
     @FXML
-    Button homepageBut, movieBut, theaterBut, employeeBut, signUpBut, signInBut, searchBut;
+    Button homepageBut, movieBut, theaterBut, employeeBut, miscBut, signUpBut, signInBut;
+
+    // miscellaneous search buttons
     @FXML
-    ComboBox theaterChoice, jobTypeChoice;
+    Button searchPastTickets,
+            searchNumTickets,
+            searchNumEmployees,
+            searchNumSeats,
+            searchRevenue;
+
+    // miscellaneous text fields
+    @FXML
+    TextField pastTixText, // past ticket purchases
+            tixSoldTheaterText, tixSoldLocationText, // number of tickets sold
+            numEmpInt, numEmpTheaterText, numEmpLocateText, // number of employees
+            numSeatsText; // number of seats left for ..
+
+    @FXML
+    ComboBox totalRevChoice;
+    ObservableList<String> mostLeastList = FXCollections.observableArrayList("Most", "Least");
 
     @FXML
     VBox miscList;
+
 
     @FXML
     private void handleButtonAction(ActionEvent event) throws IOException, SQLException {
@@ -51,9 +73,9 @@ public class MiscController implements Initializable{
             // employees.fxml
             stage = (Stage) employeeBut.getScene().getWindow();
             root = FXMLLoader.load(getClass().getResource("employee.fxml"));
-        } else if (event.getSource() == employeeBut) {// if misc. butt is clicked go to
+        } else if (event.getSource() == miscBut) {// if misc. butt is clicked go to
             // miscPage.fxml
-            stage = (Stage) employeeBut.getScene().getWindow();
+            stage = (Stage) miscBut.getScene().getWindow();
             root = FXMLLoader.load(getClass().getResource("miscPage.fxml"));
         } else if (event.getSource() == signInBut) { // if signIn butt is clicked go to movieInfo
             // signIn.fxml
@@ -72,11 +94,11 @@ public class MiscController implements Initializable{
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("Loading...");
+        totalRevChoice.setItems(mostLeastList);
 
         Drive myConn = new Drive(); // get connection
         try {
             Statement myStmt = myConn.Connect().createStatement();
-            allMisc(myStmt);
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -87,11 +109,66 @@ public class MiscController implements Initializable{
     @FXML
     private void handleMiscPage(ActionEvent event) throws IOException, SQLException {
         Drive myConn = new Drive(); // check connection
-        Statement myStmt = myConn.Connect().createStatement(); // write query
-        employeeBut.setOnAction(e -> allMisc(myStmt));
+        Statement myStmt = myConn.Connect().createStatement();
 
+        searchPastTickets.setOnAction(e -> pastTicketPurch(myStmt, pastTixText.getText()));
+        searchNumTickets.setOnAction(e -> numTixSold(myStmt, tixSoldTheaterText.getText(), tixSoldLocationText.getText()));
+        searchNumEmployees.setOnAction(e -> numOfEmployees(myStmt, numEmpInt.getText(), numEmpTheaterText.getText(), numEmpLocateText.getText()));
+        searchNumSeats.setOnAction(e -> numOfSeats(myStmt, numSeatsText.getText()));
+
+        searchRevenue.setOnAction(e -> totalRev(myStmt, (String) totalRevChoice.getValue()));
     }
 
+    // past ticket purchases for 'user input name'
+    public void pastTicketPurch(Statement myStmt, String pastTix) {
+        miscList.getChildren().clear();
+        Text title = new Text("Past ticket purchases for '" + pastTix + "'\n");
+        title.setId("fuck");
+
+        miscList.getChildren().add(title);
+    }
+
+    // Number of tickets sold by 'user select theater' at 'user select location'
+    public void numTixSold(Statement myStmt, String tixSoldTheater, String tixSoldLocation) {
+        miscList.getChildren().clear();
+        Text title = new Text("Number of tickets sold by '" + tixSoldTheater + "' at '" + tixSoldLocation +"'\n");
+        title.setId("fuck");
+
+        miscList.getChildren().add(title);
+    }
+
+    // Theaters at location with more that x employees
+    public void numOfEmployees(Statement myStmt, String numEmp, String numEmpTheater, String numEmpLocation) {
+
+
+
+        miscList.getChildren().clear();
+
+        Text title = new Text("'" + numEmpTheater + "' at '" + numEmpLocation + "' " + "with more" +
+                " " + "than "+numEmp+" employees\n");
+        title.setId("fuck");
+
+        miscList.getChildren().add(title);
+    }
+
+    // number of seats left for 'user select movie
+    public void numOfSeats(Statement myStmt, String numSeats) {
+        miscList.getChildren().clear();
+        Text title = new Text("Number of seats left for '" + numSeats + "'");
+        title.setId("fuck");
+
+        miscList.getChildren().add(title);
+    }
+
+    // total revenue in ticket purchases for each movie greater than $10.00 in order from
+    // greatest to least
+    public void totalRev(Statement stmnt, String mostLeast) {
+        miscList.getChildren().clear();
+        Text title = new Text("total revenue in ticket purchases for each movie greater than $10" + ".00 starting from '" + mostLeast + "'");
+        title.setId("fuck");
+
+        miscList.getChildren().add(title);
+    }
 
 
     @FXML
